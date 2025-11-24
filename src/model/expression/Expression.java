da05c2c18f327b5fa953783b181b4927c59a40a1
dariusp0p <dariuspop2005@gmail.com>
2025-11-24 21:18:55 +0200

    refactor: changed expressions

--- src/model/expression/Expression.java
+++ src/model/expression/Expression.java
@@ -1,9 +1,10 @@
 package model.expression;
 
+import exceptions.ExpressionException;
 import model.state.HeapTable;
 import model.state.SymbolTable;
 import model.value.Value;
 
 public interface Expression {
-    Value evaluate(SymbolTable symbolTable, HeapTable heapTable);
+    Value evaluate(SymbolTable symbolTable, HeapTable heapTable) throws ExpressionException;
 }
--- src/model/expression/ReadHeapExpression.java
+++ src/model/expression/ReadHeapExpression.java
@@ -1,27 +1,37 @@
 package model.expression;
 
+import exceptions.AdtException;
+import exceptions.ExpressionException;
 import model.state.HeapTable;
 import model.state.SymbolTable;
 import model.value.ReferenceValue;
 import model.value.Value;
 
 public record ReadHeapExpression(Expression expression) implements Expression {
-
     @Override
-    public Value evaluate(SymbolTable symbolTable, HeapTable heapTable) {
-        Value evaluatedValue = expression.evaluate(symbolTable, heapTable);
+    public Value evaluate(SymbolTable symbolTable, HeapTable heapTable) throws ExpressionException {
+        Value evaluatedValue;
+        try {
+            evaluatedValue = expression.evaluate(symbolTable, heapTable);
+        } catch (ExpressionException e) {
+            throw new ExpressionException("Failed to evaluate inner expression in rH(" + expression + ")", e);
+        }
 
         if (!(evaluatedValue instanceof ReferenceValue refValue)) {
-            throw new RuntimeException("Expression does not evaluate to a RefValue.");
+            throw new ExpressionException("rH applied to non-reference value: " + evaluatedValue);
         }
 
         int address = refValue.address();
 
-        if (!heapTable.contains(address)) {
-            throw new RuntimeException("Address " + address + " is not defined in the Heap.");
-        }
+        try {
+            if (!heapTable.contains(address)) {
+                throw new ExpressionException("Address " + address + " is not allocated in the heap.");
+            }
+            return heapTable.get(address);
 
-        return heapTable.get(address);
+        } catch (AdtException e) {
+            throw new ExpressionException("Heap access failed at address " + address, e);
+        }
     }
 
     @Override
